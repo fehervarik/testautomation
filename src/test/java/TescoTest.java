8616b552a101db4c5722df3e78340231c8997419
fehervarik <utina.84@gmail.com>
2023-06-06 09:40:15 +0200

    2023.06.05. Első nap eredménye2

--- src/test/java/TescoTest.java
+++ src/test/java/TescoTest.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
+import pages.MainPage;
 
 public class TescoTest {
 
     @Test
-    @DisplayName("Tesco login test")
+    @DisplayName("TC01_Tesco login test")
     public void testLogin() throws InterruptedException {
         LoginPage loginPage = new LoginPage();
         loginPage.openPage();
         loginPage.validateTitle();
         loginPage.validateUI();
+        MainPage mainPage = loginPage.login();
+        Thread.sleep(2000);
     }
 }
